c95eb640e125f1927342b3b0f45587a78b158505
Joshua Petitma <joshua@petit.dev>
2022-08-03 07:51:29 -0400

    Create Basic Site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      inconsolata: ["Inconsolata"],
+    },
+    extend: {
+      colors: {
+        "petit-purple": "#7F87BD",
+        "petit-light-purple": "#CFD3F1",
+        "petit-grey": "#4F4F4F",
+      },
+    },
   },
   variants: {
     extend: {},
